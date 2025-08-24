29568bb0f650b1b5337eac6a8ba5a1b99c68e7f1
eli <59635261+0x31i@users.noreply.github.com>
2025-08-24 12:48:03 -0600

    Update create_sqlite_db.sql

--- database/create_sqlite_db.sql
+++ database/create_sqlite_db.sql
@@ -22,6 +22,8 @@ insert into users values ('1','admin','admin','admin',('password'),'admin.jpg', 
 insert into users values ('2','Gordon','Brown','gordonb',('abc123'),'gordonb.jpg', DATE(), '0');
 insert into users values ('3','Hack','Me','1337',('charley'),'1337.jpg', DATE(), '0');
 insert into users values ('4','Pablo','Picasso','pablo',('letmein'),'pablo.jpg', DATE(), '0');
-insert into users values ('5','Bob','Smith','smithy',('password'),'smithy.jpg', DATE(), '0');;
+insert into users values ('5','Bob','Smith','smithy',('password'),'smithy.jpg', DATE(), '0');
+insert into users values ('6','Eli','Sims','eli',('password123'),'1337.jpg', DATE(), '0');
+insert into users values ('7','Brandon','B','brandon',('sipsip1'),'admin.jpg', DATE(), '0');;
 
 insert into guestbook values ('1', 'What a brilliant app!', 'Marcel Marceau');
