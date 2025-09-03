a006f8ed540ee54aa06610a22205276a085b59e3
eli <59635261+0x31i@users.noreply.github.com>
2025-09-03 13:30:45 -0600

    Update homepage.php

--- homepage.php
+++ homepage.php
@@ -12,19 +12,13 @@ $page[ 'page_id' ] = 'home';
 $page[ 'body' ] .= "
 <div class=\"body_padded\">
 	<h1>Welcome to Overclock!</h1>
-	<p>The hub for PC enthusiasts, builders, and performance seekers.</p>
-	<p>At Overclock, we bring together a community of hardware lovers who thrive on pushing their systems to the limit. Whether you’re building your first rig, swapping out components for the ultimate upgrade, or fine-tuning every MHz for peak performance, this is the place to share your journey.</p>
-	<ul>
-		<li>Show off your setup with photos, benchmarks, and detailed specs.</li>
-		<li>Discuss upgrades and get feedback from experienced builders.</li>
-  		<li>Discover new hardware trends and learn optimization tips from the community.</li>
-  		<li>Collaborate and connect with others who share the passion for performance.</li>
-	</ul>
+	<p>The main goal of Overclock is to be an aid for security professionals to test their skills and tools in a legal environment, help web developers better understand the processes of securing web applications and to aid both students & teachers to learn about web application security in a controlled class room environment.</p>
  	<hr />
 	<br />
 
 	<h2>Getting Started</h2>
-	<p>Head to the Forum Page where you can browse discussions or start your own.</p>
+	<p>Launch the Overclock Web Application (OCWA) to get started.</p>
+ 	<p><strong>Click the option on the main menu titled "Launch OCWA" to open the Overclock Web Application (OCWA).</strong></p>
 	<p>There is a help button at the bottom of each page, which allows you to view hints & tips for that vulnerability. There are also additional links for further background reading, which relates to that security issue.</p>
 	<hr />
 	<br />
@@ -33,7 +27,7 @@ $page[ 'body' ] .= "
 	<p>Please note, the Overclock Web Application is still in development.</p>
 	<br />
 	<h3>Disclaimer</h3>
-	<p>We do not take responsibility for the way in which any one uses this application. Overclock is a community-driven platform where members share personal experiences, advice, and opinions. We are not responsible for any hardware damage, malfunctions, or “exploding computers” caused by following advice found here. Please proceed responsibly and always research before making hardware modifications.</p>
+	<p>We do not take responsibility for the way in which any one uses this application. </p>
 	<hr />
 	<br />
 
