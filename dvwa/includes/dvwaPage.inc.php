8b2be3064aac4cb74d04c191d6fa6c081bd58f75
eli <59635261+0x31i@users.noreply.github.com>
2025-09-02 18:19:48 -0600

    Update dvwaPage.inc.php

--- dvwa/includes/dvwaPage.inc.php
+++ dvwa/includes/dvwaPage.inc.php
@@ -434,7 +434,7 @@ function dvwaHtmlEcho( $pPage ) {
 
 			<div id=\"footer\">
 
-				<p>Damn Vulnerable Web Application (DVWA)</p>
+				<p>The Overclock Web Application - v2.1.2025</p>
 				<script src='" . DVWA_WEB_PAGE_TO_ROOT . "dvwa/js/add_event_listeners.js'></script>
 
 			</div>
